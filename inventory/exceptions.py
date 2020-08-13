ad65a54e54bfba489bc5719e33f98dcd619bcd01
Jan Lajara <janlajara@gmail.com>
2020-08-13 17:48:14 +0800

    added exception for invalid expiration

--- inventory/exceptions.py
+++ inventory/exceptions.py
@@ -1,12 +1,19 @@
 class DepositTooBig(Exception):
-    def __init__(self, deposit_quantity, in_stock_quantity, limit_quantity):
-        self.message = 'Cannot deposit %s more units to %s/%s stock' % \
-                       (deposit_quantity, in_stock_quantity, limit_quantity)
+    def __init__(self, deposit_quantity, onhand_quantity, limit_quantity):
+        self.message = 'Cannot deposit or return %s more units. Stock has %s/%s units on-hand already.' % \
+                       (deposit_quantity, onhand_quantity, limit_quantity)
         super().__init__(self.message)
 
 
 class InsufficientStock(Exception):
-    def __init__(self, withdraw_quantity, in_stock_quantity, limit_quantity):
-        self.message = 'Cannot withdraw %s units from %s/%s stock' % \
-                       (withdraw_quantity, in_stock_quantity, limit_quantity)
+    def __init__(self, withdraw_quantity, available_quantity, limit_quantity):
+        self.message = 'Cannot withdraw or request %s units. Stock has insufficient quantity of %s/%s.' % \
+                       (withdraw_quantity, available_quantity, limit_quantity)
+        super().__init__(self.message)
+
+
+class InvalidExpireQuantity(Exception):
+    def __init__(self, expire_quantity, onhand_quantity):
+        self.message = 'Cannot expire %s units. Stock only has %s units on-hand.' % \
+                       (expire_quantity, onhand_quantity)
         super().__init__(self.message)

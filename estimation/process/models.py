b644042d49815a4872532b8070bc3d5cc961ba0c
Jan Lajara <janlajara@gmail.com>
2020-08-16 17:44:29 +0800

    added more functionalities

--- estimation/process/models.py
+++ estimation/process/models.py
@@ -1,16 +1,43 @@
+import math
 from django.db import models
 from djmoney.models.fields import MoneyField
 from django_measurement.models import MeasurementField
-from measurement.base import BidimensionalMeasure
-from measurement.measures import Time
-from ..measure.models import Measure, StandardUnits
+from measurement.measures import Time, Speed
+from ..measure.models import Measure, AreaSpeed, VolumeSpeed, QuantitySpeed
 
 
 # Create your models here.
+class ProcessSpeed(models.Model):
+    measure_value = models.DecimalField(decimal_places=4, max_digits=12)
+    measure_unit = models.CharField(max_length=20, choices=Measure.PRIMARY_UNITS)
+    speed_unit = models.CharField(max_length=5, choices=Measure.TIME_UNITS)
+
+    @property
+    def measure(self):
+        if self.measure_unit is not None:
+            return Measure.get_measure(self.measure_unit)
+
+    @property
+    def rate(self):
+        param = '%s__%s' % (self.measure_unit, self.speed_unit)
+        data = {param: self.measure_value}
+        rate = None
+        if self.measure == Measure.AREA:
+            rate = AreaSpeed(**data)
+        elif self.measure == Measure.QUANTITY:
+            rate = QuantitySpeed(**data)
+        elif self.measure == Measure.VOLUME:
+            rate = VolumeSpeed(**data)
+        else:
+            rate = Speed(**data)
+        return rate
+
+
 class Process(models.Model):
     name = models.CharField(max_length=40)
-    measure = models.CharField(max_length=4, choices=Measure.TYPES)
-    speed = MeasurementField(measurement=BidimensionalMeasure)
+    speed = models.OneToOneField(ProcessSpeed, on_delete=models.RESTRICT)
+    set_up = MeasurementField(measurement=Time, null=True, blank=False)
+    tear_down = MeasurementField(measurement=Time, null=True, blank=False)
 
     @property
     def flat_rate(self):
@@ -27,15 +54,29 @@ class Process(models.Model):
         hourly_rate = self._get_total_expenses(ProcessExpense.HOUR_BASED)
         return hourly_rate
 
-    def get_duration(self, measurement):
+    def get_duration(self, measurement, hours_per_day=10):
+        # factor hours for setup and teardown, multiplied by estimate num of days
+        def __compute_overall(base_duration):
+            overall = base_duration
+            if self.set_up is not None and self.tear_down is not None:
+                misc_hrs = (self.set_up.hr + self.tear_down.hr)
+                run_hours = hours_per_day - misc_hrs
+                estimate_days = math.ceil(base_duration / run_hours)
+                overall = base_duration + (misc_hrs * estimate_days)
+            return round(overall, 2)
+
         duration = 0
-        measure_uom = StandardUnits.Measure.UOMS[self.measure]
-        speed_uom = StandardUnits.Speed.UOMS[self.measure]
-        if measurement is not None and self.speed is not None:
+        measure_uom = Measure.STANDARD_UNITS[self.speed.measure]
+        speed_uom = Measure.STANDARD_SPEED_UNITS[self.speed.measure]
+
+        if measure_uom is not None and speed_uom is not None and \
+                measurement is not None and self.speed is not None:
             mval = getattr(measurement, measure_uom)
-            sval = getattr(self.speed, speed_uom)
+            sval = getattr(self.speed.rate, speed_uom)
+
             if mval is not None and sval is not None:
-                duration = mval / sval
+                duration = __compute_overall(mval / sval)
+
         return Time(hr=duration)
 
     def get_cost(self, measurement):
@@ -43,8 +84,8 @@ class Process(models.Model):
         duration = self.get_duration(measurement)
         time_cost = self.hourly_rate * duration.hr
         measure_cost = 0
-        measure_uom = StandardUnits.Measure.UOMS[self.measure]
-        if measurement is not None:
+        measure_uom = Measure.STANDARD_UNITS[self.speed.measure]
+        if measure_uom is not None and measurement is not None:
             mval = getattr(measurement, measure_uom)
             if mval is not None:
                 measure_cost = mval * self.measure_rate
--- estimation/tests.py
+++ estimation/tests.py
@@ -1,3 +1,95 @@
-from django.test import TestCase
+import pytest
+from measurement.measures import Time, Distance
+from .measure.models import Measure
+from .process.models import Process, ProcessSpeed
+
+
+@pytest.fixture
+def process_speed(db):
+    speed = ProcessSpeed.objects.create(measure_value=0.5,
+                                        measure_unit='m', speed_unit='min')
+    return speed
+
+
+@pytest.fixture
+def process(db, process_speed: ProcessSpeed):
+    process = Process.objects.create(name='GTO Printing',
+                                     speed=process_speed,
+                                     set_up=Time(hr=1),
+                                     tear_down=Time(hr=1))
+    return process
+
+
+@pytest.fixture
+def process_speed_factory(db):
+    def create_process_speed(measure_value: float, measure_unit: str, speed_unit=str):
+        process_speed = ProcessSpeed.objects.create(measure_value=measure_value,
+                                                    measure_unit=measure_unit,
+                                                    speed_unit=speed_unit)
+        return process_speed
+    return create_process_speed
+
 
 # Create your tests here.
+def test_measure__get_measure_distance(db):
+    measure = Measure.get_measure('m')
+    assert measure == Measure.DISTANCE
+
+
+def test_measure__get_measure_area(db):
+    measure = Measure.get_measure('sq_m')
+    assert measure == Measure.AREA
+
+
+def test_measure__get_measure_quantity(db):
+    measure = Measure.get_measure('pc')
+    assert measure == Measure.QUANTITY
+
+
+def test_process_speed__get_distance_rate(db, process_speed_factory):
+    process_speed = process_speed_factory(0.5, 'm', 'min')
+    assert process_speed.rate is not None
+    assert process_speed.rate.m__hr == 30
+
+
+def test_process_speed__get_area_rate(db, process_speed_factory):
+    process_speed = process_speed_factory(1, 'sq_m', 'min')
+    assert process_speed.rate.sq_m__min == 1
+
+
+def test_process_speed__get_quantity_rate(db, process_speed_factory):
+    process_speed = process_speed_factory(30, 'pc', 'min')
+    assert process_speed.rate.pc__min == 30
+
+
+def test_process_speed__get_volume_rate(db, process_speed_factory):
+    process_speed = process_speed_factory(1, 'l', 'min')
+    assert process_speed.rate.l__min == 1
+
+
+def test_process__get_duration_short(db, process: Process):
+    duration = process.get_duration(Distance(m=10))
+    # ((setup + teardown) * days) + base duration = 2.33 hrs
+    # where
+    #   = days = 1
+    #   - setup = 1
+    #   - teardown = 1
+    #   - base duration = 10 / 0.5
+    assert duration.hr == 2.33
+
+
+def test_process__get_duration_long(db, process: Process):
+    duration = process.get_duration(Distance(m=1000))
+    # ((setup + teardown) * days) + base duration = 43.33 hrs
+    # where
+    #   - days = 5
+    #   - setup = 1
+    #   - teardown = 1
+    #   - base duration = 1000 / 0.5
+    assert duration.hr == 43.33
+
+
+def test_process__no_expense(db, process: Process):
+    cost = process.get_cost(Distance(m=1))
+    assert cost == 0
+

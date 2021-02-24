ecc37f0053d6b470b12a7973e1465a73248e9765
Jan Lajara <janlajara@gmail.com>
2021-02-24 17:54:41 +0800

    populate cells from objects instead of string array

--- frontend/src/views/admin/Inventory.vue
+++ frontend/src/views/admin/Inventory.vue
@@ -10,15 +10,29 @@
                 <Section heading="Base Stock Unit" 
                     description="Individual units of measure.">
                     <Table :headers="['Unit name', 'Abbreviation', 'Plural', 
-                        'Abbreviation (Pl.)', 'Parent Alternate Stock Units']"
-                        :rows="baseUnits"/>
+                        'Abbreviation (Pl.)', 'Parent Alternate Stock Units']">
+                         <Row v-for="(unit, key) in baseUnits" :key="key">
+                            <Cell>{{unit.name}}</Cell>
+                            <Cell>{{unit.abbrev}}</Cell>
+                            <Cell>{{unit.pluralName}}</Cell>
+                            <Cell>{{unit.pluralAbbrev}}</Cell>
+                            <Cell>{{unit.altStockUnits}}</Cell>
+                        </Row>
+                    </Table>
                 </Section>
                 <hr class="my-6"/>
                 <Section heading="Alternate Stock Unit" 
                     description="Grouped units of measure.">
                     <Table :headers="['Unit name', 'Abbreviation', 'Plural', 
-                        'Abbreviation (Pl.)', 'Child Base Stock Units']"
-                        :rows="alternateUnits"/>
+                        'Abbreviation (Pl.)', 'Child Base Stock Units']">
+                        <Row v-for="(unit, key) in alternateUnits" :key="key">
+                            <Cell>{{unit.name}}</Cell>
+                            <Cell>{{unit.abbrev}}</Cell>
+                            <Cell>{{unit.pluralName}}</Cell>
+                            <Cell>{{unit.pluralAbbrev}}</Cell>
+                            <Cell>{{unit.baseStockUnits}}</Cell>
+                        </Row>
+                    </Table>
                 </Section>
             </Tab>
         </Tabs>
@@ -31,39 +45,49 @@ import Tabs from '@/components/Tabs.vue';
 import Tab from '@/components/Tab.vue';
 import Section from '@/components/Section.vue';
 import Table from '@/components/Table.vue';
+import Row from '@/components/Row.vue';
+import Cell from '@/components/Cell.vue';
 
-import {ref} from 'vue';
+import {reactive, toRefs, onBeforeMount} from 'vue';
 import {BaseStockUnitApi, AlternateStockUnitApi} from '@/utils/apis.js';
 
 export default {
     components: {
-        Page, Tabs, Tab, Section, Table
+        Page, Tabs, Tab, Section, Table, Row, Cell
     },
     setup()  {
-        const baseUnits = ref([]);
-        const alternateUnits = ref([]);
+        const units = reactive({
+            baseUnits: [{}],
+            alternateUnits: [{}]
+        })
         const populateBaseUnits = async ()=> {
-            const units = await BaseStockUnitApi.listBaseStockUnits();
-            baseUnits.value = units.map( baseUnit => (
-                [baseUnit.name, baseUnit.abbrev, 
-                baseUnit.plural_name, baseUnit.plural_abbrev, 
-                baseUnit.alternate_stock_units.join(', ')]
-            ));
+            const data = await BaseStockUnitApi.listBaseStockUnits();
+            units.baseUnits = data.map( baseUnit => ({
+                name: baseUnit.name, 
+                abbrev: baseUnit.abbrev, 
+                pluralName: baseUnit.plural_name, 
+                pluralAbbrev: baseUnit.plural_abbrev, 
+                altStockUnits: baseUnit.alternate_stock_units.join(', ')
+            }));
         }
         const populateAlternateUnits = async ()=> {
-            const units = await AlternateStockUnitApi.listAlternateStockUnits();
-            alternateUnits.value = units.map( alternateUnit => (
-                [alternateUnit.name, alternateUnit.abbrev,
-                alternateUnit.plural_name, alternateUnit.plural_abbrev, 
-                alternateUnit.base_stock_units.join(', ')]
-            ));
+            const data = await AlternateStockUnitApi.listAlternateStockUnits();
+            units.alternateUnits = data.map( alternateUnit => ({
+                name: alternateUnit.name, 
+                abbrev: alternateUnit.abbrev,
+                pluralName: alternateUnit.plural_name, 
+                pluralAbbrev: alternateUnit.plural_abbrev, 
+                baseStockUnits: alternateUnit.base_stock_units.join(', ')
+            }));
         }
 
-        populateBaseUnits();
-        populateAlternateUnits();
-
+        onBeforeMount(()=> {
+            populateBaseUnits();
+            populateAlternateUnits();
+        })
+        
         return {
-            baseUnits, alternateUnits
+            ...toRefs(units)
         }
     }
 }

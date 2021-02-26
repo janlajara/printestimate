1cb8fdc6a28d6aed59ab95feba575e0e02fd1884
Jan Lajara <janlajara@gmail.com>
2021-02-26 11:16:11 +0800

    change bg color on hover

--- frontend/src/components/Row.vue
+++ frontend/src/components/Row.vue
@@ -1,10 +1,24 @@
 <template>
-    <tr class="flex flex-wrap lg:table-row row bg-gray-50">
+    <tr class="flex flex-wrap lg:table-row row bg-gray-50"
+        :class="$props.select? 'hover:bg-secondary-light hover:bg-opacity-20 cursor-pointer': ''"
+        @click="executeSelect">
         <slot/>
     </tr>
 </template>
 <script>
 export default {
-    name: 'Row'
+    name: 'Row',
+    props: {
+        select: Function
+    },
+    setup(props){
+        const executeSelect = ()=> {
+            if (props.select)
+                props.select();
+        };
+        return {
+            executeSelect
+        }
+    }
 }
 </script>

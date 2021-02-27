ae4e147b8dac31e41318161d73d0530ff1932bd5
Jan Lajara <janlajara@gmail.com>
2021-02-27 23:10:49 +0800

    add click outside directive

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -15,4 +15,17 @@ createApp(App)
         hideProgressBar: true,
         position: 'bottom-right'
     })
+    .directive('click-outside', {
+        beforeMount(el, binding) {
+            el.clickOutsideEvent = function(event) {
+                if (!(el === event.target || el.contains(event.target))) {
+                binding.value(event, el);
+                }
+            };
+            document.body.addEventListener('click', el.clickOutsideEvent);
+        },
+        unmounted(el) {
+            document.body.removeEventListener('click', el.clickOutsideEvent);
+        }
+    })
     .mount('#app')

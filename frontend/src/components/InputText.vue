cf67358c9bcd74256c498f9a9952c2635549bc77
Jan Lajara <janlajara@gmail.com>
2021-02-27 23:10:36 +0800

    change from flex-grow to w-full

--- frontend/src/components/InputText.vue
+++ frontend/src/components/InputText.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="input">
-        <label class="input-label">{{$props.name}}</label>
+        <label class="input-label">{{$props.name}}
+            <span v-if="$props.disabled" 
+                class="material-icons text-sm text-secondary-light">lock</span>
+        </label>
         <div class="flex rounded-md shadow-sm">
             <span v-if="$props.prefix"
                 class="input-field-part rounded-l-md">
                 {{$props.prefix}}</span>
             <input :type="$props.type" :placeholder="$props.placeholder"
-                class="input-field flex-grow" :value="$props.value"
-                :class="inputBorderStyle"/>
+                class="input-field w-full" :value="$props.value"
+                :class="inputStyle" :disabled="$props.disabled"
+                :readonly="$props.readonly"/>
             <span v-if="$props.postfix"
                 class="input-field-part rounded-r-md">
                 {{$props.postfix}}</span>
@@ -33,19 +37,25 @@ export default {
             }
         },
         required: Boolean,
+        disabled: Boolean,
+        readonly: Boolean,
     },
     setup(props) {
-        const inputBorderStyle = ref()
+        const inputStyle = ref([])
 
         if (!props.prefix && !props.postfix)
-            inputBorderStyle.value = 'rounded'
+            inputStyle.value = ['rounded']
         else if (props.prefix && !props.postfix)
-            inputBorderStyle.value = 'rounded-r-md'
+            inputStyle.value = ['rounded-r-md']
         else if (!props.prefix && props.postfix)
-            inputBorderStyle.value = 'rounded-l-md'
+            inputStyle.value = ['rounded-l-md']
+
+        if (props.disabled) {
+            inputStyle.value.push('text-gray-400')
+        }
 
         return {
-            inputBorderStyle
+            inputStyle
         }
     }
 }

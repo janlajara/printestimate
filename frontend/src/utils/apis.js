2b3f31c4e54fb23f9d47b55d04098eb883336315
Jan Lajara <janlajara@gmail.com>
2021-03-04 19:49:00 +0800

    refactor and add toast notifs

--- frontend/src/utils/apis.js
+++ frontend/src/utils/apis.js
@@ -1,34 +1,58 @@
-const axios = require('axios')
+import {useToast} from 'vue-toastification'
+
+const toast = useToast();
+const showToast = (type='default', message=null)=> {
+    if (message != null)
+        return toast(message, {type});
+}
+const toastResponse = (response, success, error)=> {
+    if (response.status == 200) {
+        showToast("success", success);
+    } else {
+        showToast("error", error);
+        console.error(response);
+    }
+};
+
+const _axios = require('axios')
 const API_HOSTNAME = 'http://localhost:8000'
+const AXIOS =  {
+    execute: async (method, uri, success, error, data)=> {
+        const methods = ["post", "put", "get"];
+        const url = API_HOSTNAME + uri;
+        const config = {
+            url: url,
+            method: methods[method],
+            data
+        }
+        let response;
 
-const AXIOS_POST = async (uri, obj)=> {
-    const response = await axios.post(
-        API_HOSTNAME + uri, obj
-    );
-    return response;
+        try {
+            response = await _axios.request(config);
+            if (success != null && error != null)
+                toastResponse(response, success, error);
+        } catch (err) {
+            showToast('error', 'Failed to reach the server. Please try again.')
+            console.error(err);
+        }
+        return response;
+    },
+    POST: 0,
+    PUT: 1,
+    GET: 2,
 }
 
-const AXIOS_GET = async (uri)=> {
-    const response = await axios.get(
-        API_HOSTNAME + uri
-    );
-    return response;
-}
 
 export class InventoryApi {
     static uri = '/inventory/api/item'
 
     static async createItem(item) {
-        try {
-            const response = await AXIOS_POST(InventoryApi.uri, item);
-            console.log(response.data)
-        } catch (err) {
-            console.error(err);
-        }
+        const response = await AXIOS.execute(AXIOS.POST, InventoryApi.uri, item);
+        return response
     }
 
     static async listItems() {
-        const response = await AXIOS_GET(InventoryApi.item_uri);
+        const response = await AXIOS.execute(AXIOS.GET, InventoryApi.item_uri);
         return response.data;
     }
 }
@@ -37,13 +61,20 @@ export class BaseStockUnitApi {
     static uri = '/inventory/api/basestockunit'
 
     static async listBaseStockUnits() {
-        const response = await AXIOS_GET(BaseStockUnitApi.uri);
+        const response = await AXIOS.execute(AXIOS.GET, BaseStockUnitApi.uri);
         return response.data;
     }
 
     static async retrieveBaseStockUnit(id) {
-        const response = await AXIOS_GET(BaseStockUnitApi.uri + `/${id}`)
+        const response = await AXIOS.execute(AXIOS.GET, BaseStockUnitApi.uri + `/${id}`)
         return response.data
+    }
+
+    static async updateBaseStockUnit(id, baseStockUnit) {
+        const response = await AXIOS.execute(AXIOS.PUT, BaseStockUnitApi.uri + `/${id}/`, 
+            'Stock unit updated successfully', 'Update failed. Please try again.', baseStockUnit
+        )
+        if (response) return response.data;
     }
 }
 
@@ -51,7 +82,7 @@ export class AlternateStockUnitApi {
     static uri = '/inventory/api/alternatestockunit'
 
     static async listAlternateStockUnits() {
-        const response = await AXIOS_GET(AlternateStockUnitApi.uri);
+        const response = await AXIOS.execute(AXIOS.GET, AlternateStockUnitApi.uri);
         return response.data;
     }
 }

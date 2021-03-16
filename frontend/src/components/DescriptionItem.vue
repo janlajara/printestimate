ffea94830bb575f401fa929da1f91e9d2474c2c5
Jan Lajara <janlajara@gmail.com>
2021-03-16 14:36:48 +0800

    display None when value is empty

--- frontend/src/components/DescriptionItem.vue
+++ frontend/src/components/DescriptionItem.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <dt class="text-sm font-bold text-gray-500">{{$props.name}}</dt>
-        <dd class="mt-1">
-            {{$props.value}}
+        <dd class="mt-1" :class="($props.value == null)? 'text-black text-opacity-30 italic': ''">
+            {{($props.value == null)? 'None' : $props.value}}
         </dd>
     </div>
 </template>

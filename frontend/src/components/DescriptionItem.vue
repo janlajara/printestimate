c8a7e6c402168af151ef3ff0a9bc544d4fb5e3f3
Jan Lajara <janlajara@gmail.com>
2021-03-16 15:19:01 +0800

    remove isBlank flag

--- frontend/src/components/DescriptionItem.vue
+++ frontend/src/components/DescriptionItem.vue
@@ -1,24 +1,18 @@
 <template>
     <div>
         <dt class="text-sm font-bold text-gray-500">{{$props.name}}</dt>
-        <dd class="mt-1" :class="(isBlank)? 'text-black text-opacity-30 italic': ''">
-            {{(isBlank)? 'None' : $props.value}}
+        <dd class="mt-1" :class="($props.value == null)? 'text-black text-opacity-30 italic': ''">
+            {{($props.value == null)? 'None' : $props.value}}
         </dd>
     </div>
 </template>
 
 <script>
-import {computed} from 'vue';
-
 export default {
     props: {
         name: String,
-        value: String
-    },
-    setup(props) {
-        const isBlank = computed(()=> props.value == null || props.value.trim() == '');
-        return {
-            isBlank
+        value: {
+            type: [Number, String]
         }
     }
 }

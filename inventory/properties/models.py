372e71d8623844a2b601377c2e193bdabebb1e25
Jan Lajara <janlajara@gmail.com>
2021-03-16 14:29:44 +0800

    length_uom and width_uom are not mandatory for Line and Tape anymore

--- inventory/properties/models.py
+++ inventory/properties/models.py
@@ -45,7 +45,7 @@ class ItemProperties(PolymorphicModel):
 
 class Line(ItemProperties):
     length_value = models.FloatField(null=True, blank=True)
-    length_uom = models.CharField(max_length=30, default='m', null=True, 
+    length_uom = models.CharField(max_length=30, blank=True, null=True, 
                                   choices=Measure.UNITS[Measure.DISTANCE])
 
     @property
@@ -64,7 +64,7 @@ class Line(ItemProperties):
 
 class Tape(Line):
     width_value = models.FloatField(null=True, blank=True)
-    width_uom = models.CharField(max_length=30, default='m', null=True,
+    width_uom = models.CharField(max_length=30, blank=True, null=True,
                                  choices=Measure.UNITS[Measure.DISTANCE])
 
     @property
@@ -167,7 +167,7 @@ class Panel(Rectangle):
 
 class Liquid(ItemProperties):
     volume_value = models.FloatField(null=True, blank=True)
-    volume_uom = models.CharField(max_length=30, default='l',
+    volume_uom = models.CharField(max_length=30, blank=True, null=True,
                                   choices=Measure.UNITS[Measure.VOLUME])
 
     @property

83f781a75528f4262d646c44073b760acaa5f33c
Jan Lajara <janlajara@gmail.com>
2021-03-16 14:36:29 +0800

    hide paragraph when slot is empty

--- frontend/src/components/Button.vue
+++ frontend/src/components/Button.vue
@@ -4,7 +4,7 @@
         class="flex px-3 py-1 font-bold" 
         :class="[buttonClass, $props.icon? 'rounded-full sm:rounded' : 'rounded']">
         <Icon v-if="$props.icon" :id="$props.icon" class="w-6 text-sm"/>
-        <p class="w-full" :class="$props.icon? 'hidden sm:block' : ''" ref="buttonText">
+        <p v-if="$slots.default != null" class="w-full" :class="$props.icon? 'hidden sm:block' : ''" ref="buttonText">
             <slot/>
         </p>
     </button>
@@ -16,12 +16,14 @@ import Icon from '@/components/Icon.vue'
 
 const button_styles_active = {
     'primary': 'bg-primary hover:bg-primary-light active:bg-primary-dark',
-    'secondary': 'bg-tertiary hover:bg-tertiary-light active:bg-tertiary-dark'
+    'secondary': 'bg-tertiary hover:bg-tertiary-light active:bg-tertiary-dark',
+    'none': 'hover:text-secondary'
 }
 
 const button_styles_disabled = {
     'primary': 'bg-primary text-black text-opacity-30',
-    'secondary': 'bg-tertiary text-black text-opacity-30'
+    'secondary': 'bg-tertiary text-black text-opacity-30',
+    'none': ''
 }
 
 export default {
@@ -47,11 +49,17 @@ export default {
         }
     },
     setup(props) { 
-        const buttonClass = computed(()=> (
-            (props.disabled)? 
-                button_styles_disabled[props.color]:
-                button_styles_active[props.color]
-        ));
+        const buttonClass = computed(()=> {
+            let key = (props.color && 
+                ['primary', 'secondary'].indexOf(props.color) != -1)?
+                    props.color : 'none';
+
+            let styles = (props.disabled)? 
+                button_styles_disabled[key]:
+                button_styles_active[key]
+
+            return styles;
+        });
         return { 
             buttonClass
         }

7638a51db90a84f48a2438d17e44d585e16c5bf2
Jan Lajara <janlajara@gmail.com>
2021-03-26 16:01:03 +0800

    removed validation

--- frontend/src/utils/format.js
+++ frontend/src/utils/format.js
@@ -1,26 +1,22 @@
 
 export const formatQuantity = (quantity, unit_singular, unit_plural) => {
-    if (quantity && unit_singular && unit_plural) {
-        const number = Number(quantity)
-        const formatted = number.toLocaleString(undefined, {
-            minimumFractionDigits: 0,
-            maximumFractionDigits: 0
-        });
-        return `${formatted} ${quantity == 1? unit_singular : unit_plural}`; 
-    }
+    const number = Number(quantity)
+    const formatted = number.toLocaleString(undefined, {
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0
+    });
+    return `${formatted} ${quantity == 1? unit_singular : unit_plural}`; 
 }
 
 export const formatMoney = (amount, currency) => {
-    if (amount && currency) {
-        const number = Number(amount)
-        const formatted = number.toLocaleString("en-PH", {
-            style: 'currency',
-            currency: currency,
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2
-        });
-        return formatted;
-    }
+    const number = Number(amount)
+    const formatted = number.toLocaleString("en-PH", {
+        style: 'currency',
+        currency: currency,
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+    return formatted;
 }
 
 export const assignValue = (value, defaultValue) => {

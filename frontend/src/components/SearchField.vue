86a08dbdd273678d6be96725ebac9b6d12b674b1
Jan Lajara <janlajara@gmail.com>
2021-04-01 20:35:57 +0800

    update search field style

--- frontend/src/components/SearchField.vue
+++ frontend/src/components/SearchField.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex rounded border-b-2 border-primary-light shadow">
-        <input class="text-sm border-none focus:outline-none flex-grow" 
+    <div class="flex rounded bg-tertiary-light bg-opacity-50">
+        <input class="text-sm border-none focus:outline-none flex-grow bg-transparent" 
             type="text" v-model="search" @change="$emit('search', search)" 
             :disabled="$props.disabled" :placeholder="$props.placeholder"/>
-        <span class="material-icons mx-4 my-auto text-tertiary">search</span>
+        <span class="material-icons mx-4 my-auto">search</span>
     </div>
 </template>
 

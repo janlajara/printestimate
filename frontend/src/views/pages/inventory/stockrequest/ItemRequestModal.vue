19c30f50a470bee540ea259d03b11d60b4b247eb
Jan Lajara <janlajara@gmail.com>
2021-05-05 13:36:28 +0800

    new modal for item request

---
+++ frontend/src/views/pages/inventory/stockrequest/ItemRequestModal.vue
@@ -0,0 +1,38 @@
+<template>
+     <Modal :heading="`${$props.action == 'create'?
+            'Add' : 'Edit'} Item`" :is-open="$props.isOpen"
+        @toggle="(value)=> $emit('toggle', value)">
+        <Section>
+            <div v-if="$props.action == 'create'">
+                <InputTextLookup name="Item"/>
+                <InputText name="Quantity" type="number"/>
+            </div>
+            <div v-else>
+            </div>
+        </Section>
+    </Modal>
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue';
+import Section from '@/components/Section.vue';
+import InputText from '@/components/InputText.vue';
+import InputTextLookup from '@/components/InputTextLookup.vue';
+
+export default {
+    components: {
+        Modal, Section, InputText, InputTextLookup
+    },
+    props: {
+        isOpen:Boolean,
+        itemRequestId: Number,
+        action: {
+            type: String,
+            default: 'create',
+            validator: (value) => 
+                ['create', 'update'].indexOf(value) !== -1
+        }
+    },
+    emits: ['toggle']
+}
+</script>

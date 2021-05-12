f3195bc9e81f16b624a5af6bb2a8ac3a9675e577
Jan Lajara <janlajara@gmail.com>
2021-05-12 15:07:41 +0800

    fixed col widths layout option

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -1,10 +1,14 @@
 <template>
     <div class="my-4 rounded-md overflow-hidden">
-        <table class="table-auto w-full">
+        <table class="w-full" 
+            :class="$props.layout == 'auto' ?
+                'lg:table-auto' : 'lg:table-fixed'">
             <thead class="hidden lg:table-header-group">
                 <tr class="flex flex-wrap lg:table-row row bg-primary-light bg-opacity-50">
                     <th v-for="(header, key) in $props.headers" :key="key" 
-                        class="px-3 py-3 sm:px-6 text-left text-xs">{{header}}</th>
+                        class="px-3 py-3 sm:px-6 text-left text-xs"
+                        :class="columnsWidth ? columnsWidth[key] : null">
+                        {{header}}</th>
                 </tr>
             </thead>
             <tbody class="bg-white divide-y divide-gray-200">
@@ -39,17 +43,31 @@ export default {
         Row, Cell
     },
     props: {
+        layout: {
+            type: String,
+            default: 'auto',
+            validator: value => ['auto', 'fixed'].includes(value)
+        },
         headers: {
             type: Array,
             required: true
         },
+        colsWidth: Array,
         rows: Array,
         loader: Boolean
     },
     setup(props) {
         const columnCount = computed(()=> (props.headers? props.headers.length: 0));
         return {
-            columnCount
+            columnCount,
+            columnsWidth: computed(()=> {
+                let cw = null;
+                if (props.layout == 'fixed' && props.colsWidth && 
+                    props.colsWidth.length == props.headers.length) {
+                        cw = props.colsWidth;
+                }
+                return cw;
+            })
         }
     }
 }

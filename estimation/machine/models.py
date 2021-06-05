736498f0391dedb881ccac63709151d9eb1efe06
Jan Lajara <janlajara@gmail.com>
2021-06-05 18:33:14 +0800

    major refactoring

--- estimation/machine/models.py
+++ estimation/machine/models.py
@@ -1,39 +1,137 @@
 from django.db import models
-from django_measurement.models import MeasurementField
+from core.utils.shapes import Rectangle
+from core.utils.measures import Measure
 from measurement.measures import Distance
-from polymorphic.models import PolymorphicModel
-from ..exceptions import SheetSizeInvalid
 
 
-class Machine(PolymorphicModel):
+class MachineManager(models.Manager):
+    def create_machine(self, **kwargs):
+        mapping = {
+            Machine.PRESS : PressMachine,
+            Machine.OTHERS : Machine
+        }
+        type = kwargs.get('type', Machine.OTHERS)
+        clazz = mapping[type]
+        machine = clazz.objects.create(**kwargs)
+        return machine
+
+
+class Machine(models.Model):
+    PRESS = 'press'
+    OTHERS = 'others'
+    TYPES = [
+        (PRESS, 'Press'),
+        (OTHERS, 'Others'),
+    ]
+    objects = MachineManager()
     name = models.CharField(max_length=100)
-
-    def validate_input(self, **kwargs):
-        pass
+    type = models.CharField(max_length=15, choices=TYPES, null=False, blank=False)
 
 
-class SheetfedPress(Machine):
-    min_sheet_length = MeasurementField(measurement=Distance)
-    max_sheet_length = MeasurementField(measurement=Distance)
-    min_sheet_width = MeasurementField(measurement=Distance)
-    max_sheet_width = MeasurementField(measurement=Distance)
+class PressMachine(Machine):
+    min_sheet_length = models.FloatField(blank=True, default=0)
+    max_sheet_length = models.FloatField(blank=True, default=0)
+    min_sheet_width = models.FloatField(blank=True, default=0)
+    max_sheet_width = models.FloatField(blank=True, default=0)
+    uom = models.CharField(max_length=30, default='mm',
+        choices=Measure.UNITS[Measure.DISTANCE])
 
-    def validate_input(self, **kwargs):
-        runsheet_width = kwargs.get('runsheet_width')
-        runsheet_length = kwargs.get('runsheet_length')
-        if runsheet_width is not None and runsheet_length is not None:
-            length_valid = self.min_sheet_length.m <= runsheet_length.m \
-                           <= self.max_sheet_length.m
-            width_valid = self.min_sheet_width.m <= runsheet_width.m \
-                          <= self.max_sheet_width.m
-            if not (length_valid and width_valid):
-                expected_min = "%s x %s" % (self.min_sheet_width, self.min_sheet_length)
-                expected_max = "%s x %s" % (self.max_sheet_width, self.max_sheet_length)
-                actual = "%s x %s" % (runsheet_width, runsheet_length)
-                raise SheetSizeInvalid(expected_min, expected_max, actual)
+    def add_parent_sheet(self, width, length, size_uom, 
+        padding_top=None, padding_right=None,
+        padding_bottom=None, padding_left=None):
+
+        parent = ParentSheet.objects.create(machine=self,
+            width_value=width, length_value=length, size_uom=size_uom,
+            padding_top=padding_top, padding_right=padding_right,
+            padding_bottom=padding_bottom, padding_left=padding_left)
+
+        return parent
 
 
-class RollfedPress(Machine):
-    min_sheet_width = MeasurementField(measurement=Distance)
-    max_sheet_width = MeasurementField(measurement=Distance)
+class ParentSheet(Rectangle):
+    machine = models.ForeignKey(PressMachine, on_delete=models.CASCADE, 
+        related_name='parent_sheets')
+    padding_top = models.FloatField(null=True, blank=True, default=0)
+    padding_right = models.FloatField(null=True, blank=True, default=0)
+    padding_bottom = models.FloatField(null=True, blank=True, default=0)
+    padding_left = models.FloatField(null=True, blank=True, default=0)
 
+    @property
+    def pack_width(self):
+        return self.width_value - self.padding_x
+
+    @property
+    def pack_length(self):
+        return self.length_value - self.padding_y
+
+    @property
+    def padding(self):
+        return (self.padding_top, self.padding_right, 
+            self.padding_bottom, self.padding_left)
+    
+    @property
+    def padding_x(self):
+        return self.padding_right + self.padding_left
+    
+    @property
+    def padding_y(self):
+        return self.padding_top + self.padding_bottom
+
+    def add_child_sheet(self, width, length, size_uom, 
+        margin_top=None, margin_right=None,
+        margin_bottom=None, margin_left=None):
+
+        child = ChildSheet.objects.create(parent=self,
+            width_value=width, length_value=length, size_uom=size_uom,
+            margin_top=margin_top, margin_right=margin_right,
+            margin_bottom=margin_bottom, margin_left=margin_left)
+            
+        return child
+
+
+class ChildSheet(Rectangle):
+    parent = models.ForeignKey(ParentSheet, on_delete=models.CASCADE,
+        related_name='child_sheets')
+    margin_top = models.FloatField(blank=True, default=0)
+    margin_right = models.FloatField(blank=True, default=0)
+    margin_bottom = models.FloatField(blank=True, default=0)
+    margin_left = models.FloatField(blank=True, default=0)
+
+    @property
+    def pack_width(self):
+        return self.width_value + self.margin_x
+    
+    @property
+    def pack_length(self):
+        return self.length_value + self.margin_y
+
+    @property
+    def margin(self):
+        return (self.margin_top, self.margin_right, 
+            self.margin_bottom, self.margin_left)
+
+    @property
+    def margin_x(self):
+        return self.margin_left + self.margin_right
+    
+    @property
+    def margin_y(self):
+        return self.margin_top + self.margin_bottom
+
+    @property
+    def packer(self):
+        return self.parent.pack(self)
+
+    @property
+    def count(self):
+        return len(self.packer)
+
+    @property
+    def usage(self):
+        used_area = self.pack_width * self.pack_length * self.count
+        total_area = self.parent.pack_width * self.parent.pack_length
+        return (used_area / total_area)
+
+    @property
+    def wastage(self):
+        return 1 - self.usage
--- estimation/machine/tests.py
+++ estimation/machine/tests.py
@@ -1,16 +1,27 @@
 import pytest
-from measurement.measures import Distance
-from .models import SheetfedPress
-from ..exceptions import SheetSizeInvalid
+from .models import Machine
+from core.utils.measures import Measure
 
 @pytest.fixture
 def gto_machine(db):
-    return SheetfedPress.objects.create(name='GTO Press',
-                                           min_sheet_length=Distance(inch=15), max_sheet_length=Distance(inch=22),
-                                           min_sheet_width=Distance(inch=15), max_sheet_width=Distance(inch=22))
+    return Machine.objects.create_machine(name='GTO Press', 
+        type=Machine.PRESS, uom='inch')
 
 
-def test_machine__validate_input(db, gto_machine):
-    with pytest.raises(SheetSizeInvalid):
-        gto_machine.validate_input(runsheet_length=Distance(inch=14),
-                                   runsheet_width=Distance(inch=14))
+def test_machine__add_parent_sheet(db, gto_machine):
+    parent_sheet = gto_machine.add_parent_sheet(17, 22, 'inch', 1, 1, 1, 1)
+    assert parent_sheet.width.inch == 17
+    assert parent_sheet.length.inch == 22
+    assert parent_sheet.padding == (1, 1, 1, 1)
+    assert parent_sheet.padding_x == 2
+    assert parent_sheet.padding_y == 2
+
+def test_parent_sheet__add_child_sheet(db, gto_machine):
+    parent_sheet = gto_machine.add_parent_sheet(21, 26, 'inch', 1, 1, 1, 1)
+    child_sheet = parent_sheet.add_child_sheet(8.5, 11, 'inch', 0.5, 0.5, 0.5, 0.5)
+    assert child_sheet.width.inch == 8.5
+    assert child_sheet.length.inch == 11
+    assert child_sheet.margin == (0.5, 0.5, 0.5, 0.5)
+    assert child_sheet.count == 4
+    assert child_sheet.usage == 1
+    assert child_sheet.wastage == 0

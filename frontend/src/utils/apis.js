3f1f1bc8e6e417b77dbeccf45d30bbf296d23375
Jan Lajara <janlajara@gmail.com>
2021-07-05 17:58:35 +0800

    add activity expense, activity, operation, operation steps apis

--- frontend/src/utils/apis.js
+++ frontend/src/utils/apis.js
@@ -334,3 +334,93 @@ export class WorkstationApi {
             'Workstation deleted successfully.', 'Delete failed. Please try again.');
     }
 }
+
+
+export class ActivityExpenseApi {
+    static uri = '/estimation/api/activityexpenses'
+
+    static async retrieveActivityExpense(id) {
+        const response = await AXIOS.execute(AXIOS.GET, ActivityExpenseApi.uri + `/${id}`)
+        return response.data
+    }
+
+    static async updateActivityExpense(id, activityExpense) {
+        const response = await AXIOS.execute(AXIOS.PUT, ActivityExpenseApi.uri + `/${id}/`, 
+            'Activity expense updated successfully.', 'Update failed. Please try again.', activityExpense
+        )
+        if (response) return response.data;
+    }
+
+    static async deleteActivityExpense(id) {
+        await AXIOS.execute(AXIOS.DELETE, ActivityExpenseApi.uri + `/${id}/`, 
+            'Activity expense deleted successfully.', 'Delete failed. Please try again.');
+    }
+}
+
+export class ActivityApi {
+    static uri = '/estimation/api/activities'
+
+    static async retrieveActivity(id) {
+        const response = await AXIOS.execute(AXIOS.GET, ActivityApi.uri + `/${id}`)
+        return response.data
+    }
+
+    static async updateActivity(id, activity) {
+        const response = await AXIOS.execute(AXIOS.PUT, ActivityApi.uri + `/${id}/`, 
+            'Activity updated successfully.', 'Update failed. Please try again.', activity
+        )
+        if (response) return response.data;
+    }
+
+    static async deleteActivity(id) {
+        await AXIOS.execute(AXIOS.DELETE, ActivityApi.uri + `/${id}/`, 
+            'Activity deleted successfully.', 'Delete failed. Please try again.');
+    }
+}
+
+export class OperationApi {
+    static uri = '/estimation/api/operations'
+
+    static async retrieveOperation(id) {
+        const response = await AXIOS.execute(AXIOS.GET, OperationApi.uri + `/${id}`)
+        return response.data
+    }
+
+    static async retrieveOperationSteps(id) {
+        const response = await AXIOS.execute(AXIOS.GET, OperationApi.uri + `/${id}/steps`)
+        return response.data
+    }
+
+    static async updateOperation(id, operation) {
+        const response = await AXIOS.execute(AXIOS.PUT, OperationApi.uri + `/${id}/`, 
+            'Operation updated successfully.', 'Update failed. Please try again.', operation
+        )
+        if (response) return response.data;
+    }
+
+    static async deleteOperation(id) {
+        await AXIOS.execute(AXIOS.DELETE, OperationApi.uri + `/${id}/`, 
+            'Operation deleted successfully.', 'Delete failed. Please try again.');
+    }
+}
+
+
+export class OperationStepApi {
+    static uri = '/estimation/api/operationsteps'
+
+    static async retrieveOperationStep(id) {
+        const response = await AXIOS.execute(AXIOS.GET, OperationStepApi.uri + `/${id}`)
+        return response.data
+    }
+
+    static async updateOperationStep(id, operationStep) {
+        const response = await AXIOS.execute(AXIOS.PUT, OperationStepApi.uri + `/${id}/`, 
+            'Operation step updated successfully.', 'Update failed. Please try again.', operationStep)
+        if (response) return response.data;
+    }
+
+    static async deleteOperationStep(id) {
+        await AXIOS.execute(AXIOS.DELETE, OperationStepApi.uri + `/${id}/`, 
+            'Operation step deleted successfully.', 'Delete failed. Please try again.');
+    }
+}

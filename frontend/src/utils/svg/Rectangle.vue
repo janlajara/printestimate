d6c93387c4a88e6a1949885f8b8872069bd44587
Jan Lajara <janlajara@gmail.com>
2021-07-18 15:19:23 +0800

    add svg components

---
+++ frontend/src/utils/svg/Rectangle.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <svg :id="`rect-${state.id}`"></svg>
+    </div>
+</template>
+<script>
+import Snap from 'snapsvg-cjs';
+import {v4 as uuid} from 'uuid';
+import {reactive, onMounted} from 'vue';
+
+export default {
+    setup(){
+        const state = reactive({
+            id: uuid()
+        });
+        console.log(state.id);
+
+        onMounted(()=> {
+            const elementId = `#rect-${state.id}`;
+            const snap = Snap(elementId);
+
+            if (snap) {
+                const circle = snap.circle(150, 150, 100);
+                circle.attr({
+                    fill: "#bada55",
+                    stroke: "#000",
+                    strokeWidth: 5
+                });
+            }
+        })
+
+        return {
+            state
+        }
+    }
+}
+</script>

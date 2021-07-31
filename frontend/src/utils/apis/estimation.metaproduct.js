4e8e34636b46f3592f7356cc327f9751c6526d5e
Jan Lajara <janlajara@gmail.com>
2021-07-31 16:01:38 +0800

    separate estimation APIs

---
+++ frontend/src/utils/apis/estimation.metaproduct.js
@@ -0,0 +1,71 @@
+import {AXIOS} from '@/utils/apis/core.js'
+
+export class MetaProductApi {
+    static uri = '/estimation/api/metaproducts'
+
+    static async listMetaProducts() {
+        const response = await AXIOS.execute(AXIOS.GET, MetaProductApi.uri);
+        if (response) return response.data;
+    }
+
+    static async createMetaProduct(metaProduct) {
+        const response = await AXIOS.execute(AXIOS.POST, 
+            MetaProductApi.uri, 'Product Meta created successfully.', 
+            'Create failed. Please try again.', metaProduct);
+        return response;
+    }
+
+    static async retrieveMetaProduct(id) {
+        const response = await AXIOS.execute(AXIOS.GET, MetaProductApi.uri + `/${id}/`)
+        return response.data
+    }
+
+    static async updateMetaProduct(id, metaProduct) {
+        const response = await AXIOS.execute(AXIOS.PUT, 
+            MetaProductApi.uri + `/${id}/`, 'Product Meta updated successfully.', 
+            'Update failed. Please try again.', metaProduct)
+        if (response) return response.data;
+    }
+
+    static async deleteMetaProduct(id) {
+        await AXIOS.execute(AXIOS.DELETE, MetaProductApi.uri + `/${id}/`, 
+            'Product Meta deleted successfully.', 'Delete failed. Please try again.');
+    }
+
+    static async retrieveMetaProductComponents(id) {
+        const response = await AXIOS.execute(AXIOS.GET, 
+            MetaProductApi.uri + `/${id}/metacomponents`);
+        if (response) return response.data;
+    }
+
+    static async createMetaProductComponent(id, metaComponent) {
+        const response = await AXIOS.execute(AXIOS.POST, 
+            MetaProductApi.uri + `/${id}/metacomponents`, 
+            'Component created successfully.', 'Create failed. Please try again.', 
+            metaComponent);
+        return response;
+    }
+}
+
+
+export class MetaComponentApi {
+    static uri = '/estimation/api/metacomponents'
+
+    static async retrieveMetaComponent(id) {
+        const response = await AXIOS.execute(AXIOS.GET, MetaComponentApi.uri + `/${id}/`)
+        return response.data
+    }
+
+    static async updateMetaComponent(id, metaComponent) {
+        const response = await AXIOS.execute(AXIOS.PUT, 
+            MetaComponentApi.uri + `/${id}/`, 'Component updated successfully.', 
+            'Update failed. Please try again.', metaComponent)
+        if (response) return response.data;
+    }
+
+    static async deleteMetaComponent(id) {
+        await AXIOS.execute(AXIOS.DELETE, MetaComponentApi.uri + `/${id}/`, 
+            'Component deleted successfully.', 'Delete failed. Please try again.');
+    }
+
+}

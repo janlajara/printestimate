ff19e851cbd64218c426247738a11f515170cb7c
Jan Lajara <janlajara@gmail.com>
2021-08-07 11:20:53 +0800

    remove unused code

--- estimation/product/models.py
+++ estimation/product/models.py
@@ -6,38 +6,9 @@ from polymorphic.models import PolymorphicModel
 from polymorphic.managers import PolymorphicManager
 
 
-class Component(models.Model):
-    name = models.CharField(max_length=40)
-    type = models.CharField(max_length=15, choices=Item.TYPES)
-
-    def add_material(self, name, quantity, **kwargs):
-        material = Material.objects.create_material(component=self, 
-            quantity=quantity, name=name, **kwargs)
-        return material
-
-
-class MaterialManager(PolymorphicManager):
-    def create_material(self, component, quantity, name=None, **kwargs):
-        mapping = {
-            Item.TAPE: TapeMaterial,
-            Item.LINE: LineMaterial,
-            Item.PAPER: PaperMaterial,
-            Item.PANEL: PanelMaterial,
-            Item.LIQUID: LiquidMaterial,
-            Item.OTHER: Material
-        }
-        clazz = mapping.get(component.type, Material)
-        material = clazz.objects.create(name=name, component=component,
-            quantity=quantity, **kwargs)
-        return material
-
-
 class Material(PolymorphicModel, Shape):
-    objects = MaterialManager()
     material_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20, default='Material')
-    component = models.ForeignKey(Component, on_delete=models.CASCADE,
-        related_name='materials')
     quantity = models.IntegerField(default=1)
 
     @property

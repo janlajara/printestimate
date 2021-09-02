c6164f304f15f6f219df8684a4c1b40d220f4d1b
Jan Lajara <janlajara@gmail.com>
2021-09-02 13:49:48 +0800

    add template urls

--- estimation/urls.py
+++ estimation/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('estimation.process.urls')),
     path('', include('estimation.machine.urls')),
-    path('', include('estimation.metaproduct.urls'))
+    path('', include('estimation.metaproduct.urls')),
+    path('', include('estimation.template.urls'))
 ]

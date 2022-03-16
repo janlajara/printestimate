5b569a82f8324475c6fdad63c10896264e3dcde0
Jan Lajara <janlajara@gmail.com>
2022-03-16 14:48:32 +0800

    revise costing measure units url

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from core import views
 
 urlpatterns = [
     # Costing Measures List View
-    path('api/utils/costingmeasures', views.CostingMeasuresListView.as_view())
+    path('api/utils/costingmeasures/units', views.CostingMeasuresListView.as_view())
 ]

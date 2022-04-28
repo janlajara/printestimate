bb3ba11079cfade9ab517ba9a03c76bd57cac06d
Jan Lajara <janlajara@gmail.com>
2022-04-28 17:37:43 +0800

    add screen layout prefix for print

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      screens: {
+        print: {'raw': 'print'},
+      },
       colors: {
         primary: {
           light: '#FCA5A5', 

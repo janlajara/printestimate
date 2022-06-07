42b7e82eb1d7145a3182db4753bcc9a4c4b28a67
Jan Lajara <janlajara@gmail.com>
2022-06-07 14:59:19 +0800

    open link to new tab

--- frontend/src/views/Footer.vue
+++ frontend/src/views/Footer.vue
@@ -1,7 +1,7 @@
 <template>
     <footer class="py-8 px-8 md:px-16 text-center md:text-left bg-pattern-graph">
         <p class="text-sm">Printestimate for 
-            <a href="https://www.bjlprintingpress.com"
+            <a href="https://www.bjlprintingpress.com"  target="_blank"
                 class="text-primary-dark font-medium underline">
                 BJ Lajara Printing Press</a>
         </p> 

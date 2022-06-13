c19ec1759fc53965d3de48ed5f0c2cca6e4952ff
Jan Lajara <janlajara@gmail.com>
2022-06-13 20:48:03 +0800

    fileio urls

--- printestimate/urls.py
+++ printestimate/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
     path('inventory/', include('inventory.urls')),
-    path('estimation/', include('estimation.urls'))
+    path('estimation/', include('estimation.urls')),
+    path('fileio/', include('fileio.urls'))
 ]

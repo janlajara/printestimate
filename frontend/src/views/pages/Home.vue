55fe26b8256f9b7d9aa7b9db61aaf983fd0f565f
Jan Lajara <janlajara@gmail.com>
2022-06-14 17:59:24 +0800

    hero section and features

--- frontend/src/views/pages/Home.vue
+++ frontend/src/views/pages/Home.vue
@@ -2,17 +2,37 @@
     <div>
         <Page>
             <div class="md:w-2/3">
-                <h1 class="text-2xl sm:text-4xl font-extrabold mb-6">
-                    Made with 
-                    <span class="text-secondary">Vue 3</span> and 
-                    <span class="text-primary">Tailwind CSS</span>
+                <h1 class="text-2xl sm:text-4xl font-extrabold mb-4">
+                    Estimate print jobs 
+                    <span class="text-primary">instantly</span>
                 </h1>
                 <p class="mb-6 text-justify">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Printestimate is an application especially designed for printers
+                    that follow the <a href="http://budgeted-hourly-cost-rates.com/" 
+                        class="text-secondary-dark">budgeted hourly rates (BHRs)</a> 
+                    type of costing. 
                 </p>
-                <p class="text-justify">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+            </div>
+            <div>
+                <div class="mt-14">
+                    <dl class="space-y-10 md:space-y-0 md:grid md:gap-x-8 md:gap-y-10">
+                        <div v-for="feature in features" :key="feature.title" class="relative">
+                            <dt>
+                                <div class="absolute flex items-center justify-center h-12 w-12 
+                                    rounded-md bg-primary text-white">
+                                    <span class="h-6 w-6 material-icons" aria-hidden="true">
+                                        {{feature.icon}}
+                                    </span>
+                                </div>
+                                <p class="ml-16 text-base leading-6 font-medium text-gray-900">
+                                    {{feature.title}}</p>
+                            </dt>
+                            <dd class="mt-1 ml-16 text-sm text-gray-500">
+                                {{feature.description}}
+                            </dd>
+                        </div>
+                    </dl>
+                </div>
             </div>
         </Page>
     </div>
@@ -24,6 +44,22 @@ import Page from '@/components/Page.vue'
 export default {
     components: {
         Page
-    }    
+    },
+    setup(){
+        const features = [
+            {title: 'Estimates within seconds',
+                icon: 'schedule',
+                description: 'Quickly create your estimates after a one-time setup.'},
+            {title: 'Material costing included',
+                icon: 'request_quote',
+                description: 'Automatically calculate for the number of raw materials to be consumed.'},
+            {title: 'Multiple quantity estimates',
+                icon: '123',
+                description: 'View estimates for different quantities all on the same page.'},
+        ]
+        return {
+            features
+        }
+    }
 }
 </script>

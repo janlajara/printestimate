b9a5d929324bcb486467d6c8b19b9a9b1f454750
Jan Lajara <janlajara@gmail.com>
2022-06-15 20:07:39 +0800

    separate dockerfiles for frontend and backend

---
+++ Dockerfile.vue
@@ -0,0 +1,18 @@
+# VUE
+FROM node:14-slim as frontend
+WORKDIR /frontend
+
+# Install dependencies
+COPY ./frontend/package*.json ./
+RUN npm install
+
+# Copy project
+COPY ./frontend .
+RUN npm run build
+
+
+# NGINX
+FROM nginx
+COPY --from=frontend /frontend/dist /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]

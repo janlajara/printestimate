3775b1fb32e8574ce1904732868fd865b1ed2611
Jan Lajara <janlajara@gmail.com>
2022-07-28 16:02:52 +0800

    add viewbox to linemeasure

--- frontend/src/utils/svg/LineMeasure.vue
+++ frontend/src/utils/svg/LineMeasure.vue
@@ -1,10 +1,10 @@
 <template>
-    <svg>
-        <!--line 
+    <svg :viewBox="`0 0 ${$props.viewBoxWidth} ${$props.viewBoxLength}`">
+        <line 
             :x1="$props.x1" :y1="$props.y1" 
             :x2="$props.x2" :y2="$props.y2" 
             :style="state.style" />
-        <circle :fill="$props.stroke" :r="$props.endpointRadius"
+        <!--circle :fill="$props.stroke" :r="$props.endpointRadius"
             :cx="$props.x1" :cy="$props.y1"/>
         <circle :fill="$props.stroke" :r="$props.endpointRadius"
             :cx="$props.x2" :cy="$props.y2"/-->
@@ -25,7 +25,9 @@ export default {
         x2: {type: Number, default: 0}, 
         y2: {type: Number, default: 0},
         stroke: {type: String, default: 'black'},
-        strokeWidth: {type: Number, default: 1},
+        strokeWidth: {type: Number, default: 0.1},
+        viewBoxWidth: {type: Number, default: 0}, 
+        viewBoxLength: {type: Number, default: 0},
         dashed: Boolean,
         textSize: {type: Number, default: 1},
         text: {type: [Number, String]}
@@ -35,8 +37,8 @@ export default {
             style: computed(()=>({
                 stroke: props.stroke,
                 strokeWidth: props.strokeWidth,
-                'stroke-dasharray': props.dashed ? '4,2' : 'none',
-                'vector-effect': 'non-scaling-stroke'
+                //'stroke-dasharray': props.dashed ? '4,2' : 'none',
+                //'vector-effect': 'non-scaling-stroke'
             })),
         });
         return {
